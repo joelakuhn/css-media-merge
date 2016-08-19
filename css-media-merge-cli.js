dd47dfc4c7145c149e038d30c3ea795ff5f989ee
Joel Kuhn <joelakuhn@gmail.com>
2016-08-19 11:16:38 -0400

    Added usage

--- css-media-merge-cli.js
+++ css-media-merge-cli.js
@@ -11,10 +11,20 @@ var options = {
 var inFile = null;
 
 
+function show_help() {
+	console.log('usage: ' + "\n" +
+							'    css-media-merge [--mobile-first] <file>' + "\n" +
+							'    css-media-merge [--mobile-first] < <file>' + "\n");
+}
+
 
 for (var i=0; i<args.length; i++) {
 	if (args[i] == '--mobile-first') {
 		options.mobile_first = true;
+	}
+	if (args[i] == '--help' || args[i] == '-h') {
+		show_help();
+		process.exit();
 	}
 	else {
 		inFile = args[i];
